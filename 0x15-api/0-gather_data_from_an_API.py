582ebcce452897d586146637afb0fdbdf75f7064
adentintime <arixbizz@gmail.com>
2018-08-28 04:39:42 +0000

    Update 0, won't execute on import

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,27 +7,27 @@
 from sys import argv
 import requests
 
+if __name__ == "__main__":
+    employee_id = argv[1]
 
-employee_id = argv[1]
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(employee_id))
 
-r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                 .format(employee_id))
+    EMPLOYEE_NAME = r.json().get('name')
 
-EMPLOYEE_NAME = r.json().get('name')
+    r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                     .format(employee_id))
 
-r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                 .format(employee_id))
+    NUMBER_OF_DONE_TASKS = [x.get('completed') for x in r.json()].count(True)
 
-NUMBER_OF_DONE_TASKS = [x.get('completed') for x in r.json()].count(True)
+    TOTAL_NUMBER_OF_TASKS = len(r.json())
 
-TOTAL_NUMBER_OF_TASKS = len(r.json())
+    print('Employee {EMPLOYEE_NAME} is done with '
+          'tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):'
+          .format(EMPLOYEE_NAME=EMPLOYEE_NAME,
+                  NUMBER_OF_DONE_TASKS=NUMBER_OF_DONE_TASKS,
+                  TOTAL_NUMBER_OF_TASKS=TOTAL_NUMBER_OF_TASKS))
 
-print('Employee {EMPLOYEE_NAME} is done with '
-      'tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):'
-      .format(EMPLOYEE_NAME=EMPLOYEE_NAME,
-              NUMBER_OF_DONE_TASKS=NUMBER_OF_DONE_TASKS,
-              TOTAL_NUMBER_OF_TASKS=TOTAL_NUMBER_OF_TASKS))
-
-for x in r.json():
-    if x.get('completed') is True:
-        print('\t ' + x.get('title'))
+    for x in r.json():
+        if x.get('completed') is True:
+            print('\t ' + x.get('title'))
